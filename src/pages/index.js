ef99c7376a6a61039b3797386548fed5fc10a8d3
Katherine Trammell <ktrammell@gmail.com>
2017-08-15 21:00:26 -0400

    WEB-6 - spent way too much time trying to learn Gatsby - added font-awesome - added bootstrap - started HTML structure - started base styling - will need to look closely at responsive design on all screen sizes and adjust

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,80 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
 
-const IndexPage = () =>
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
 
-export default IndexPage
+import backtoback from '../assets/backtoback.png'
+import css from '../assets/css3.png'
+import react from '../assets/reactNE.png'
+import html from '../assets/HTML5_Badge_512.png'
+import logo from '../assets/logo-title-light.png'
+import todd from '../assets/todd_smart.jpg'
+
+
+import '../layouts/main.css';
+import '../layouts/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
+
+
+export default () =>
+<div>
+    <section className="smart-binary">
+        <h2>What is Smart Binary?</h2>
+        <div className="row">
+            <div className="col-sm-6">
+            <p>I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .N</p>
+            </div>
+            <div className="col-sm-6">
+                <img src={backtoback} alt="Logo" />
+            </div>
+        </div>
+    </section>
+    <section className="what-we-do_wrapper">
+        <div className="what-we-do">
+            <h2>What do we do?</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                <p>I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .N</p>
+                </div>
+                <div className="col-sm-6">
+                    <div className="row">
+                        <img src={logo} alt="Logo" className="col-sm-6" />
+                        <img src={html} alt="html5" className="col-sm-6" />
+                        <img src={react} alt="react" className="col-sm-6" />
+                        <img src={css} alt="css3" className="col-sm-6" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section className="founders">
+        <h2>Meet the Founders</h2>
+        <h4>Smart Binary is basically run by superheros</h4>
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a href="" className="nav-link">Founder 1</a></li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">Founder 2</a></li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">Founder 3</a></li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">Founder 4</a></li>
+            </ul>
+        </nav>
+        <div className="row">
+            <div className="col-sm-6">
+                <img src={todd} alt="Todd Smart" />
+            </div>
+            <div className="col-sm-6">
+                <h5>Todd Smart</h5>
+                <h6>Partner & Senior Consultant</h6>
+                <p>I am a professional software engineer with over two decades of Full Stack development experience focused on JavaScript, Java/J2EE, and .NET.</p>
+                <div className="social-icons">
+                    <i className="fa fa-twitter"/>
+                    <i className="fa fa-reddit"/>
+                    <i className="fa fa-linkedin-square"/>
+                    <i className="fa fa-dribbble"/>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</div>
---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography"
+import bootstrapTheme from "typography-theme-bootstrap"
+
+const typography = new Typography({
+    baseFontSize: '12px',
+    baseLineHeight: 1.45,
+    headerFontFamily: ['Source Sans Pro', 'sans-serif'],
+    bodyFontFamily: ['Source Sans Pro', 'sans-serif'],
+})
+
+export default typography
