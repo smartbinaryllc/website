83ba52a151e0100f78f0564f26fc1d59574ab4c1
Brad Bohen <bbohen@gmail.com>
2017-09-22 10:58:44 -0400

    WEB-10 Fixed deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  pathPrefix: `/smartbinary.github.io`,
+  pathPrefix: `/website`,
   plugins: [
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-sass`,
-      {
-          resolve: `gatsby-plugin-typography`,
-          options: {
-              pathToConfigModule: `src/utils/typography.js`,
-          },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
       },
+    },
   ],
   siteMetadata: {
     title: `Smart Binary, LLC`,
