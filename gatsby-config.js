2977484fa205cd423206ed2083524121879f739f
Todd Smart <todd.smart@smartbinary.com>
2017-09-22 11:01:28 -0400

    WEB-10 updates for gh-pages deployment (#9)
    * WEB-10 updates for gh-pages deployment
    
    * WEB-10 Fixed deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
+  pathPrefix: `/website`,
   plugins: [
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-sass`,
-      
-      {
-          resolve: `gatsby-plugin-typography`,
-          options: {
-              pathToConfigModule: `src/utils/typography.js`,
-          },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
       },
+    },
   ],
+  siteMetadata: {
+    title: `Smart Binary, LLC`,
+  },
 }
